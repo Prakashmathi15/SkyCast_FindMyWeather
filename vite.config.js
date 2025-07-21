053b578754d421a193fa9f3a13f346081752c93b
M.PRAKASH. <118350045+Prakashmathi15@users.noreply.github.com>
2025-07-21 19:49:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/SkyCast_FindMyWeather/', // <-- MUST match your repository name
+  base: '/SkyCast_FindMyWeather/', 
   plugins: [react()],
-});
+})
